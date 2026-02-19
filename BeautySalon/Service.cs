24634217578a8b0004834b839f7b4fa0627b32db
Анна <katanamy.meow@gmail.com>
2026-02-19 23:17:15 +0700

    // TODO:время услуги ,проверка цены . инфо услуги

--- BeautySalon/Service.cs
+++ BeautySalon/Service.cs
@@ -5,7 +5,7 @@
     public decimal Price { get; }
     public int Duration { get; }
 
-    public Service(int id, string name, decimal price, int duration)
+    public Service(int id, string name, decimal price, int duration)//
     {
         Id = id;
         Name = name;
